50ed559755a760af2ae916bae968c8892bd209bb
9999 <71588143+EssenceShift@users.noreply.github.com>
2021-09-09 01:07:11 +0700

    Update getUrlList.js

--- getUrlList.js
+++ getUrlList.js
@@ -2,27 +2,45 @@ const puppeteer = require('puppeteer');
 const axios = require('axios').default;
 var parseString = require('xml2js').parseString;
 
+function typeCheck(siteName){
+    const type = [
+        ['https://hashtelegraph.com/'],
+        ['https://bitnovosti.com/'],
+        ['https://forklog.com/','https://beincrypto.ru/','http://cryptoconsulting.info/'], // post max
+        ['https://bits.media/'], // random
+        ['https://vc.ru/']
+    ]
 
-function siteMapCheck(i) {
-    if(i == 0)
-        return true;
-    return false;
+    let typeReturn = -1
+    for(let i = 0; i < type.length;i++) {
+        type[i].forEach(item => {
+            if( item === siteName )
+                typeReturn = i
+        })
+    }
+    return typeReturn
 }
 
 (async () => {
-    const addressFirst =  'http://cryptoconsulting.info/ru/'
+    const addressFirst =  'https://vc.ru/'
     const addressSecond = addressFirst + 'sitemap.xml'
     const xmlSiteMap = await axios.get(addressSecond)
     let xmlDate
     await parseString(xmlSiteMap.data, function (err, res) {
          xmlData = res.sitemapindex.sitemap;
     });
-
-    for(let i =0; i < xmlData.length; i++){
-        if(siteMapCheck(i))
-             xmlDate = xmlData[i].loc[0];
+    let siteType = typeCheck(addressFirst)
+    if(siteType === 1 || siteType === 0 || siteType === 4){
+        for(let i =0; i < xmlData.length; i++){
+            if(i === (siteType)){
+                xmlDate = xmlData[i].loc[0];
+            }
+        }
+        console.log(xmlDate)
     }
-    console.log(xmlDate)
+    if(siteType === -1 || siteType === 2 || siteType === 5 ){
+        console.log("TODO")
+    }
     const xmlNews = await axios.get(xmlDate)
     let result
     await parseString(xmlNews.data, function (err, res) {
